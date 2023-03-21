0bb1fdc3eb150e1794c577b5749990a2e369da0b
LeZach <48424078+zf-development@users.noreply.github.com>
2023-03-21 17:39:33 -0400

    Refactor and removed doubled code (Wenmo)

--- client/wenmo.lua
+++ client/wenmo.lua
@@ -4,22 +4,16 @@ RegisterNUICallback('wenmo_givemoney_toID', function(data)
     TriggerServerEvent('qb-phone:server:wenmo_givemoney_toID', data)
 end)
 
-RegisterNetEvent('hud:client:OnMoneyChange', function(type, amount, isMinus, reason)
-    if type == "bank" then
-        if isMinus then
-            SendNUIMessage({
-                action = "ChangeMoney_Wenmo",
-                Color = "#f5a15b",
-                Amount = "-$"..amount,
-                Reason = reason or "",
-            })
-        else
-            SendNUIMessage({
-                action = "ChangeMoney_Wenmo",
-                Color = "#8ee074",
-                Amount = "+$"..amount,
-                Reason = reason or "",
-            })
-        end
-    end
+RegisterNetEvent('QBCore:Client:OnMoneyChange', function(account, amount, type, reason)
+    if not account == 'bank' then return end
+    local color = type == 'remove' and "#f5a15b" or "#8ee074"
+    local amount = type == 'remove' and "- $" .. amount or "+ $" .. amount
+    if reason == 'unknown' then reason = false end
+    
+    SendNUIMessage({
+        action = "ChangeMoney_Wenmo",
+        color = color,
+        amount = amount,
+        reason = reason,
+    })
 end)
--- server/wenmo.lua
+++ server/wenmo.lua
@@ -1,33 +1,29 @@
-local QBCore = exports['qb-core']:GetCoreObject()
-
 RegisterNetEvent('qb-phone:server:wenmo_givemoney_toID', function(data)
     local src = source
-    local Ply = QBCore.Functions.GetPlayer(src)
-    local OtherPly = QBCore.Functions.GetPlayer(tonumber(data.ID))
-    local Amount = tonumber(data.Amount)
-    local Reason = data.Reason
+    local Player = QBCore.Functions.GetPlayer(src)
+    local Target = QBCore.Functions.GetPlayer(tonumber(data.id))
+    local amount = tonumber(data.amount)
+    local reason = data.reason
 
-    if src == tonumber(data.ID) then return end
+    if src == tonumber(data.id) then return end
+    if not Target then return PhoneNotify(src, 'Wenmo', 'The recipient is not available', 'error', 'fas fa-exclamation-circle', '#e76f51') end
 
-    if not OtherPly then return TriggerClientEvent('QBCore:Notify', src, 'Player not Online', "error") end
-
-    local txt = "Wenmo: "..Reason
-    
-    if Ply.PlayerData.money.bank >= Amount then
-        Ply.Functions.RemoveMoney('bank', Amount, txt)
-        OtherPly.Functions.AddMoney('bank', Amount, txt)
+    local txt = "Wenmo: " .. reason
+    if Player.PlayerData.money.bank >= amount then
+        Player.Functions.RemoveMoney('bank', amount, txt)
+        Target.Functions.AddMoney('bank', amount, txt)
 
         if Config.RenewedBanking then
-            local cid = Ply.PlayerData.citizenid
-            local name = ("%s %s"):format(Ply.PlayerData.charinfo.firstname, Ply.PlayerData.charinfo.lastname)
+            local pCitizen = Player.PlayerData.citizenid
+            local name = ("%s %s"):format(Player.PlayerData.charinfo.firstname, Player.PlayerData.charinfo.lastname)
 
-            local cid2 = OtherPly.PlayerData.citizenid
-            local name2 = ("%s %s"):format(OtherPly.PlayerData.charinfo.firstname, OtherPly.PlayerData.charinfo.lastname)
+            local tCitizen = Target.PlayerData.citizenid
+            local name2 = ("%s %s"):format(Target.PlayerData.charinfo.firstname, Target.PlayerData.charinfo.lastname)
 
-            exports['Renewed-Banking']:handleTransaction(cid, "Wenmo Transaction", Amount, txt, name2, name, "withdraw")
-            exports['Renewed-Banking']:handleTransaction(cid2, "Wenmo Transaction", Amount, txt, name, name2, "deposit")
+            exports['Renewed-Banking']:handleTransaction(pCitizen, "Wenmo Transaction", amount, txt, name2, name, "withdraw")
+            exports['Renewed-Banking']:handleTransaction(tCitizen, "Wenmo Transaction", amount, txt, name, name2, "deposit")
         end
     else
-        TriggerClientEvent("QBCore:Notify", src, 'You don\'t have enough money!', "error") -- replace this with Phone Notify
+        PhoneNotify(src, 'Wenmo', 'You don\'t have enough money', "fas fa-exclamation-circle", "#e76f51")
     end
 end)
