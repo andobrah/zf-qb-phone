49ee17036a80a3462e47a2ef9da8e688c08717ed
LeZach <48424078+zf-development@users.noreply.github.com>
2023-04-05 20:53:27 -0400

    Added Jobs update when Core is Updated.

--- client/_utils.lua
+++ client/_utils.lua
@@ -2,4 +2,9 @@ QBCore = exports[Config.Exports.Core]:GetCoreObject()
 
 RegisterNetEvent('QBCore:Client:UpdateObject', function()
 	QBCore = exports[Config.Exports.Core]:GetCoreObject()
+	
+	SendNUIMessage({
+		action = "UpdateChat",
+		jobs = QBCore.Shared.Jobs
+	    })
 end)
